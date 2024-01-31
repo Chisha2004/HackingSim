e3df7357646bedb3baae892444c3d04a4d96b6d3
Tim <tmatzenauer@hotmail.de>
2024-01-31 18:34:59 +0100

    Implemented cd Command

--- src/main/java/com/timprogrammiert/Terminal/Commands/ChangeDirectory/ChangeDirectory.java
+++ src/main/java/com/timprogrammiert/Terminal/Commands/ChangeDirectory/ChangeDirectory.java
@@ -1,6 +1,13 @@
 package com.timprogrammiert.Terminal.Commands.ChangeDirectory;
 
 import com.timprogrammiert.Computer.Computer;
+import com.timprogrammiert.Exceptions.FileNotFoundException;
+import com.timprogrammiert.Exceptions.NotADirectoryException;
+import com.timprogrammiert.Filesystem.BaseFile;
+import com.timprogrammiert.Filesystem.Directories.DirectoryObject;
+import com.timprogrammiert.Filesystem.Filesystem;
+import com.timprogrammiert.Filesystem.Path.Path;
+import com.timprogrammiert.Filesystem.Path.PathResolver;
 import com.timprogrammiert.Terminal.Commands.ICommand;
 
 import java.util.Arrays;
@@ -11,8 +18,42 @@ import java.util.Arrays;
  * Version: 1.0
  */
 public class ChangeDirectory implements ICommand {
+    Filesystem filesystem;
     @Override
     public String run(Computer computer, String[] args) {
-        return Arrays.toString(args);
+        this.filesystem = computer.getOperatingSystem().getFilesystem();
+        return changeDirectory(args[0]);
     }
+
+    /**
+     * Changes the current directory in the filesystem to the specified path.
+     *
+     * @param pathString The string representation of the desired path.
+     * @return An empty string if the directory change is successful, or an error message if an exception occurs.
+     */
+    private String changeDirectory(String pathString) {
+        Path path = new Path(pathString);
+        PathResolver pathResolver = new PathResolver(filesystem);
+        DirectoryObject dirToCd;
+
+        try {
+            // Resolve the path to obtain the corresponding BaseFile.
+            BaseFile resolvedObject = pathResolver.resolvePath(path);
+
+            if (resolvedObject instanceof DirectoryObject) {
+                dirToCd = (DirectoryObject) resolvedObject;
+
+                filesystem.setCurrentDirectory(dirToCd);
+            } else {
+                // Throw an exception if the resolved object is not a directory.
+                throw new NotADirectoryException(pathString);
+            }
+        } catch (FileNotFoundException | NotADirectoryException e) {
+            return e.getMessage();
+        }
+
+        // Return an empty string indicating a successful directory change.
+        return "";
+    }
+
 }
