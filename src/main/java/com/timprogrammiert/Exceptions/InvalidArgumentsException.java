8a6261c370959ae9d4b0ff5978fa792c0ef2b50a
Tim <tmatzenauer@hotmail.de>
2024-02-02 18:34:00 +0100

    Implemented InvalidArgumentsException
    Chmod now Checks if current User has permission execute the command - depends on File or Directory

---
+++ src/main/java/com/timprogrammiert/Exceptions/InvalidArgumentsException.java
@@ -0,0 +1,12 @@
+package com.timprogrammiert.Exceptions;
+
+/**
+ * Author : Tim
+ * Date: 02.02.2024
+ * Version: 1.0
+ */
+public class InvalidArgumentsException extends Exception{
+    public InvalidArgumentsException() {
+        super("Invalid Arguments");
+    }
+}
--- src/main/java/com/timprogrammiert/Terminal/Commands/Chmod/Chmod.java
+++ src/main/java/com/timprogrammiert/Terminal/Commands/Chmod/Chmod.java
@@ -2,8 +2,11 @@ package com.timprogrammiert.Terminal.Commands.Chmod;
 
 import com.timprogrammiert.Computer.Computer;
 import com.timprogrammiert.Exceptions.FileNotFoundException;
+import com.timprogrammiert.Exceptions.InvalidArgumentsException;
 import com.timprogrammiert.Exceptions.NotADirectoryException;
+import com.timprogrammiert.Exceptions.PermissionDeniedException;
 import com.timprogrammiert.Filesystem.BaseFile;
+import com.timprogrammiert.Filesystem.EnumFileTypes;
 import com.timprogrammiert.Filesystem.Path.Path;
 import com.timprogrammiert.Filesystem.Path.PathResolver;
 import com.timprogrammiert.Filesystem.Permissions.PermissionChecker;
@@ -30,12 +33,13 @@ public class Chmod implements ICommand {
         argList.removeIf(String::isBlank);
         try {
             return handleArguments(argList);
-        } catch (NotADirectoryException | FileNotFoundException e) {
+        } catch (NotADirectoryException | FileNotFoundException | InvalidArgumentsException | PermissionDeniedException e) {
             return e.getMessage();
         }
     }
-    private String handleArguments(List<String> argList) throws NotADirectoryException, FileNotFoundException {
+    private String handleArguments(List<String> argList) throws NotADirectoryException, FileNotFoundException, InvalidArgumentsException, PermissionDeniedException {
         String permissionCode = argList.getFirst();
+        if(!(isValid(permissionCode))){throw new InvalidArgumentsException();}
         String filePath = argList.get(1);
 
         Path path = new Path(filePath);
@@ -43,8 +47,33 @@ public class Chmod implements ICommand {
         BaseFile file = pathResolver.resolvePath(path);
 
         PermissionChecker pemChecker = new PermissionChecker(file.getPermissions(), computer.getOperatingSystem().getCurrentUser());
-
-
+        if(file.getFileType().equals(EnumFileTypes.File) && pemChecker.isCanWrite()){
+            // Change FILE PERMISSIONS
+        } else if (file.getFileType().equals(EnumFileTypes.Directory) && pemChecker.isCanWrite() && pemChecker.isCanExecute()) {
+            // CHANGE DIRECTORY PERMISSIONS
+        }else {
+            throw new PermissionDeniedException();
+        }
         return "";
+    }
+
+    private boolean isValid(String permissionArguments) {
+        // Checks only if the Input type is Correct, not the actual numeric values
+        if(permissionArguments.equals("+x")){
+            return true;
+        } else if ((permissionArguments.length() == 3 && isNumeric(permissionArguments))) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
