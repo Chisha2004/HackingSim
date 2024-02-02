219351a09de36358d1dad645ac3b8724278ccc92
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2024-02-02 20:15:36 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+# conf.py
+
+# Project information
+project = 'HackingSim ReadTheDocs'
+author = 'Tim'
+
+# Extensions
+extensions = [
+    'sphinx.ext.autodoc',  # Automatically generate API documentation from docstrings
+    'sphinx.ext.viewcode',  # Add "View source" links to your documentation
+]
