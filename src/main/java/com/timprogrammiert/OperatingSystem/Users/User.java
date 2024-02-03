4e25ee98332017f3cb2ee480ae97c2d65a155afc
Tim <tmatzenauer@hotmail.de>
2024-02-03 18:36:20 +0100

    User objects now contains a Map of Groups the object is member of

--- src/main/java/com/timprogrammiert/OperatingSystem/Users/User.java
+++ src/main/java/com/timprogrammiert/OperatingSystem/Users/User.java
@@ -1,5 +1,11 @@
 package com.timprogrammiert.OperatingSystem.Users;
 
+import com.timprogrammiert.OperatingSystem.Groups.Group;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Author : Tim
  * Date: 28.01.2024
@@ -8,9 +14,11 @@ package com.timprogrammiert.OperatingSystem.Users;
 public class User {
     String password;
     String userName;
+    Map<String, Group> groupList;
     public User(String userName, String password){
         this.password = password;
         this.userName = userName;
+        this.groupList = new HashMap<>();
     }
 
     public String getPassword() {
@@ -23,5 +31,12 @@ public class User {
 
     public String getUserName() {
         return userName;
+    }
+    public Group getSpecificGroup(String groupName){
+        return groupList.get(groupName);
+    }
+
+    public List<Group> getAllGroups(){
+        return (List<Group>) groupList.values();
     }
 }
